b3cd36eae9722d0fc9adde85ac0854267b6f907f
hanjunpeng <1358271059@qq.com>
2018-10-20 21:05:33 +0800

    commit
    提交代码

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import { app, BrowserWindow, Menu, MenuItem } from 'electron';
+import { app, BrowserWindow, Menu, MenuItem, ipcMain } from 'electron';
+import { appMenuTemplate } from './appMenu';
 
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
+//是否可以安全退出
+let safeExit = false;
 
 let mainWindow;
-console.log(Menu)
+
 const createWindow = () => {
   mainWindow = new BrowserWindow({
     width: 800,
@@ -14,17 +17,59 @@ const createWindow = () => {
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-
+  // 打开客户端调试工具
   mainWindow.webContents.openDevTools();
+  // 增加主菜单
+  const menu = Menu.buildFromTemplate(appMenuTemplate)
+  menu.items[0].submenu.append(new MenuItem({ //menu.items获取是的主菜单一级菜单的菜单数组，menu.items[0]在这里就是第1个File菜单对象，在其子菜单submenu中添加新的子菜单
+    label: "New",
+    click(){
+      mainWindow.webContents.send('action', 'new'); //点击后向主页渲染进程发送“新建文件”的命令
+    },
+    accelerator: 'CmdOrCtrl+N' //快捷键：Ctrl+N
+  }));
+  //在New菜单后面添加名为Open的同级菜单
+  menu.items[0].submenu.append(new MenuItem({
+    label: "Open",
+    click(){
+      mainWindow.webContents.send('action', 'open'); //点击后向主页渲染进程发送“打开文件”的命令
+    },
+    accelerator: 'CmdOrCtrl+O' //快捷键：Ctrl+O
+  }));
+  //再添加一个名为Save的同级菜单
+  menu.items[0].submenu.append(new MenuItem({
+    label: "Save",
+    click(){
+      mainWindow.webContents.send('action', 'save'); //点击后向主页渲染进程发送“保存文件”的命令
+    },
+    accelerator: 'CmdOrCtrl+S' //快捷键：Ctrl+S
+  }));
+  //添加一个分隔符
+  menu.items[0].submenu.append(new MenuItem({
+    type: 'separator'
+  }));
+  //再添加一个名为Exit的同级菜单
+  menu.items[0].submenu.append(new MenuItem({
+    role: 'quit'
+  }));
+  Menu.setApplicationMenu(menu); //注意：这个代码要放到菜单添加完成之后，否则会造成新增菜单的快捷键无效
 
+  mainWindow.on('close', (e) => {
+    if(!safeExit){
+      e.preventDefault();
+      mainWindow.webContents.send('action', 'exiting');
+    }
+  });
 
   mainWindow.on('closed', () => {
-    mainWindow = null;
-  });
+    mainWindow = null
+  })
 };
 
+// electron 初始化完成后
 app.on('ready', createWindow);
 
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
@@ -34,5 +79,17 @@ app.on('window-all-closed', () => {
 app.on('activate', () => {
   if (mainWindow === null) {
     createWindow();
+  }
+});
+
+//监听与渲染进程的通信
+ipcMain.on('reqaction', (event, arg) => {
+  switch(arg){
+    case 'exit':
+      //做点其它操作：比如记录窗口大小、位置等，下次启动时自动使用这些设置；不过因为这里（主进程）无法访问localStorage，这些数据需要使用其它的方式来保存和加载，这里就不作演示了。这里推荐一个相关的工具类库，可以使用它在主进程中保存加载配置数据：https://github.com/sindresorhus/electron-store
+      //...
+      safeExit=true;
+      app.quit();//退出程序
+      break;
   }
 });
